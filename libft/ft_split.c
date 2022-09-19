8d5239304ace3843de34ac7f32f1560b17b3570b
Leonardo Barrenechea Marins <barrenechea89marins@gmail.com>
2022-09-19 15:31:55 +0200

    libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,96 +6,12 @@
 /*   By: lbarrene <lbarrene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 16:23:52 by lbarrene          #+#    #+#             */
-/*   Updated: 2022/09/19 15:25:25 by lbarrene         ###   ########.fr       */
+/*   Updated: 2022/09/19 15:31:18 by lbarrene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
-
-size_t	ft_strlen(const char *arg)
-{
-	size_t	size;
-
-	size = 0;
-	if (arg[size] == 0)
-		return (0);
-	while (arg[size])
-		size++;
-	return (size);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	src_len;
-
-	if (!dst || !src)
-		return (0);
-	src_len = ft_strlen(src);
-	i = 0;
-	if (dstsize != 0)
-	{
-		while (src[i] != '\0' && i < dstsize - 1)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (src_len);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
-	size_t	a;
-
-	i = 0;
-	a = 0;
-	if ((dst == src) || n == 0)
-		return (dst);
-	while (i < n)
-	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[a];
-		i++;
-		a++;
-	}
-	n = '\0';
-	return (dst);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	char	*str;
-	size_t	len;
-
-	len = ft_strlen(s1);
-	str = (char *)malloc((len + 1) * sizeof(char));
-	if (!str)
-		return (0);
-	else
-		ft_memcpy(str, s1, ft_strlen(s1));
-	*(str + len) = '\0';
-	return (str);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char			*str;
-
-	if (!s)
-		return (0);
-	if (start > ft_strlen(s))
-		return (ft_strdup(""));
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (0);
-	ft_strlcpy(str, &s[start], len + 1);
-	return (str);
-}
 
 static int	count_words(char const *s, char c)
 {
@@ -118,19 +34,6 @@ static int	count_words(char const *s, char c)
 	return (i);
 }
 
-static char	*word_split(char const *str, int stt, int fnsh)
-{
-	char	*word;
-	int		i;
-
-	i = 0;
-	word = malloc((fnsh - stt + 1) * sizeof(char));
-	while (stt < fnsh)
-		word[i++] = str[stt++];
-	word[i] = '\0';
-	return (word);
-}
-
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
@@ -138,7 +41,7 @@ char	**ft_split(char const *s, char c)
 	int		index;
 	char	**split;
 
-split = malloc((count_words(s, c) + 1) * sizeof(char *));
+	split = malloc((count_words(s, c) + 1) * sizeof(char *));
 	if (!s || !split)
 		return (0);
 	i = 0;
