ecda19b62e3968a3e08547e41365b4905718e3b4
LeoBarrenechea <barrenechea89marins@gmail.com>
2022-09-27 15:36:52 +0200

    GNL

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,93 +6,11 @@
 /*   By: lbarrene <lbarrene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 16:19:17 by lbarrene          #+#    #+#             */
-/*   Updated: 2022/09/27 15:34:51 by lbarrene         ###   ########.fr       */
+/*   Updated: 2022/09/27 15:36:39 by lbarrene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
-	size_t	a;
-
-	i = 0;
-	a = 0;
-	if ((dst == src) || n == 0)
-		return (dst);
-	while (i < n)
-	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[a];
-		i++;
-		a++;
-	}
-	n = '\0';
-	return (dst);
-}
-
-size_t	ft_strlen(const char *arg)
-{
-	size_t	size;
-
-	size = 0;
-	if (arg[size] == 0)
-		return (0);
-	while (arg[size])
-		size++;
-	return (size);
-}
-
-char	*ft_strrchr(const char *str, int c)
-{
-	int	a;
-
-	a = 0;
-	while (str[a])
-		a++;
-	while (0 <= a)
-	{
-		if (str[a] == (char)c)
-			return ((char *)str + a);
-		a--;
-	}
-	return (NULL);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	char	*str;
-	size_t	len;
-
-	len = ft_strlen(s1);
-	str = (char *)malloc((len + 1) * sizeof(char));
-	if (!str)
-		return (0);
-	else
-		ft_memcpy(str, s1, ft_strlen(s1));
-	*(str + len) = '\0';
-	return (str);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	i;
-	size_t	a;
-	char	*str;
-
-	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	a = 0;
-	while (s1[i])
-		str[a++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		str[a++] = s2[i++];
-	str[a] = '\0';
-	return (str);
-}
 
  char	*read_save_fd(int fd, char *saved)
 {
