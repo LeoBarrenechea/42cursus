ecde62e6fcb1ea8751e2f06f571b8cf905feb00c
LeoBarrenechea <barrenechea89marins@gmail.com>
2022-10-31 17:55:20 +0100

    swap done

--- push_swap/ft_swap.c
+++ push_swap/ft_swap.c
@@ -6,62 +6,11 @@
 /*   By: lbarrene <lbarrene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 14:31:10 by lbarrene          #+#    #+#             */
-/*   Updated: 2022/10/31 17:48:10 by lbarrene         ###   ########.fr       */
+/*   Updated: 2022/10/31 17:54:57 by lbarrene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-t_stak	*ft_addnew(int arg)
-{
-	t_stak	*new;
-
-	new = (t_stak *)malloc(sizeof(t_stak));
-	if (new != NULL)
-	{
-		new -> num = arg;
-		new -> next = NULL;
-	}
-	return (new);
-}
-
-void	ft_insertend(t_head *peek, int arg)
-{
-	t_stak	*aux;
-	t_stak	*new;
-
-	new = ft_addnew(arg);
-	if (peek->peek == NULL)
-		peek->peek = new;
-	else
-	{
-		aux = peek->peek;
-		while (aux->next)
-			aux = aux->next;
-		aux->next = new;
-	}
-	peek->len++;
-	peek->etail = new;
-}
-
-void	ft_insertinit(t_head *peek, int arg)
-{
-	t_stak	*new;
-
-	new = ft_addnew(arg);
-	peek->peek = new;
-	peek->len++;
-}
-
-t_head	*ft_addheader(void)
-{
-	t_head	*peek;
-
-	peek = (t_head *)malloc(sizeof(t_head));
-	peek->len = 0;
-	peek->peek = NULL;
-	return (peek);
-}
 
 void	ft_swap(t_head *peek)
 {
