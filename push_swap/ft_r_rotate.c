99dcd6ddc3d31c89a22917373efe158b8772cc44
LeoBarrenechea <barrenechea89marins@gmail.com>
2022-11-07 15:35:55 +0100

    push_swap

--- push_swap/ft_r_rotate.c
+++ push_swap/ft_r_rotate.c
@@ -6,8 +6,47 @@
 /*   By: lbarrene <lbarrene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 17:55:20 by lbarrene          #+#    #+#             */
-/*   Updated: 2022/10/26 17:55:34 by lbarrene         ###   ########.fr       */
+/*   Updated: 2022/11/07 12:39:41 by lbarrene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_r_rotate(t_head *peek)
+{
+	t_stak	*aux;
+
+	if (peek->len > 1)
+	{
+		aux = peek->peek;
+		while (peek->peek->next->next)
+			peek->peek = peek->peek->next;
+		peek->etail = peek->peek;
+		peek->peek = peek->etail->next;
+		peek->etail->next = NULL;
+		peek->peek->next = aux;
+	}
+}
+
+int main(void)
+{
+	t_head	*peek;
+	int		i;
+
+	peek = ft_addheader();
+	i = 0;
+	int a[] = {50};
+	while (i < 1)
+	{
+		ft_insertend(peek, a[i]);
+		i++;
+	}
+	i = 0;
+	ft_r_rotate(peek);
+	while (peek->peek)
+	{
+		printf("[%i]  |  %d\n", i, peek->peek->num);
+		peek->peek = peek->peek->next;
+		i++;
+	}
+}
